febf8283e63a5f9573985defc94c1ceaa187ede9
Elwadee3 <wdeei2018@gmail.com>
2026-05-11 08:42:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pyvis.network import Network
+#from pyvis.network import Network
 import streamlit.components.v1 as components
 import tempfile
 import html
