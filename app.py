b994a3132156d6ff87469c79f9b11cc7aecee60c
Elwadee3 <wdeei2018@gmail.com>
2026-05-17 11:51:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def create_graph(selected_id, source_text, mappings):
         color="#1687d9", 
         size=300, 
         shape="circle", 
-        font={'color': 'white', 'size': 26, 'bold': True}
+        font={'color': 'white', 'size': 40, 'bold': True}
     )
 
     for idx, item in enumerate(mappings):
